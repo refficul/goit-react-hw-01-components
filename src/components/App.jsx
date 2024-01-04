02d74327af4306f8a47cfada87668f2b8a89528a
refficul <vic.w.toria.q@gmail.com>
2024-01-04 03:49:10 +0200

    make changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import user from 'dates/user.json'
+import downloads from 'dates/downloads.json'
+import friends from 'dates/friends.json'
+import transactions from 'dates/transactions.json'
+
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+import styles from './App.module.css'
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <main className={styles.main}>
+      <Profile user={user} />
+      <Statistics download={downloads} />
+      <FriendList friends={friends} />
+      <TransactionHistory item={transactions} />
+    </main>
   );
 };
